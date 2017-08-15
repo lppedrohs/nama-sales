092ccfb03e9793d11a59e3ad2bec9fd1f5a9eece
Pedro Henrique <lppedrohs@gmail.com>
2017-08-15 01:04:41 -0300

    Removing implicit required on belongs_to

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,7 +1,7 @@
 class Sale < ApplicationRecord
   belongs_to :provider
   belongs_to :buyer
-  belongs_to :importation
+  belongs_to :importation, required: false
 
   validates_presence_of :price, :quantity, :address, :provider, :buyer
 end
